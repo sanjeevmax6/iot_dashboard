abbf058e93fa328406bfac2414e2575d275a4584
Sanjeev Vijayakumar <sanvegas2001@gmail.com>
2026-03-23 23:17:56 -0400

    added all resources for deployment

---
+++ infra/cdk/lib/ecr-stack.ts
@@ -0,0 +1,29 @@
+import * as cdk from "aws-cdk-lib";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import { Construct } from "constructs";
+
+export class EcrStack extends cdk.Stack {
+  public readonly repository: ecr.Repository;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.repository = new ecr.Repository(this, "BackendRepo", {
+      repositoryName: "iot-dashboard-backend",
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // added this manually for cleaner setup for this project. In a real world use case, would need to retain for backup (for rollbacks and availability)
+      emptyOnDelete: true,
+      imageScanOnPush: true,
+      lifecycleRules: [
+        {
+          maxImageCount: 10,
+          description: "Keep only the last 10 images",
+        },
+      ],
+    });
+
+    new cdk.CfnOutput(this, "RepositoryUri", {
+      value: this.repository.repositoryUri,
+      description: 'ECR repository URI — set ECR_REPOSITORY GitHub variable to "iot-dashboard-backend"',
+    });
+  }
+}
---
+++ infra/cdk/lib/vpc-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
+
+export class VpcStack extends cdk.Stack {
+  public readonly vpc: ec2.Vpc;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.vpc = new ec2.Vpc(this, "Vpc", {
+      maxAzs: 2, // 2 is ideal for the scale of this project mostly
+      natGateways: 1, // needed to connect private instances with the public instances & internet
+    });
+  }
+}
